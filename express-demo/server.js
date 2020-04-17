84395cca93a7bb02d9c55c27f5df2744f993a811
Cássia <cascrisbern@gmail.com>
2020-04-17 17:15:19 -0300

    delete route added

--- express-demo/server.js
+++ express-demo/server.js
@@ -12,16 +12,23 @@ const courses = [
 ]
 
 server.get('/', (req, res) => {
-  return res.send("hello world!")
+  return res.send("Hello World! Smile you are in the index page!")
 })
 
 server.get('/api/courses', (req, res) => {
-  return res.send(courses)
+  return res.send(courses) //* It sends all the courses I have registered so far.
 })
 
 server.get('/api/courses/:id', (req,res) => {
   const course = courses.find(course => course.id === parseInt(req.params.id))
-  if(!course) return res.status(404).send('Course not found')
+  // courses are an array. 
+  // find method takes one object (course) of this array per time.
+  // you access this object (course) that find took by dot notation.
+  // and compare it with id typed on the url. 
+  // because this id is a string, you need to transform it to number first.
+  // then the comparation will succeed.
+
+  if(!course) return res.status(404).send('It seems we do not have this course.')
 
   return res.send(course)
 })
@@ -30,7 +37,7 @@ server.post('/api/courses', (req,res) => {
   if(!req.body.name || req.body.length < 3) {
   return res.status(400).send("name is required and should be minimum of 3 characters")
 }
-  
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
