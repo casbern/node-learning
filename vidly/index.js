7c71a775bff9b93dd1cde7020a20c178aafdd6d3
Cássia <cascrisbern@gmail.com>
2020-04-18 18:32:20 -0300

    add routes

--- vidly/index.js
+++ vidly/index.js
@@ -4,10 +4,18 @@ const server = express()
 
 server.use(express.json())
 
-const genres = [
-  {id: 1, name: "comedy"},
-  {id: 2, name: "romance"},
-  {id: 3, name: "terror"}
+const genres = [{
+    id: 1,
+    name: "comedy"
+  },
+  {
+    id: 2,
+    name: "romance"
+  },
+  {
+    id: 3,
+    name: "terror"
+  }
 ]
 
 server.get("/", (req, res) => {
@@ -21,11 +29,63 @@ server.get("/api/genres", (req, res) => {
 server.get("/api/genres/:id", (req, res) => {
   const genre = genres.find(genre => genre.id === parseInt(req.params.id))
 
-  if(!genre) return res.status(400).send("It seems we do not have this id you are looking for.")
+  if (!genre) return res.status(400).send("It seems we do not have this id you are looking for.")
 
   return res.send(genre)
 })
 
+server.post("/api/genres", (req, res) => {
+  //INPUT VALIDATION WITH JOI
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  })
+
+  const result = schema.validate(req.body)
+
+  if (result.error) {
+    console.log(result.error)
+    return res.status(400).send(result.error.details[0].message)
+  }
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  }
+
+  genres.push(genre)
+  return res.send(genre)
+})
+
+server.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id))
+  if(!genre) return res.status(404).send('This genre was not found')
+
+   //INPUT VALIDATION WITH JOI
+   const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  })
+
+  const result = schema.validate(req.body)
+
+  if(result.error) {
+    console.log(result.error)
+    return res.status(400).send(result.error.details[0].message)
+  }
+
+  genre.name = req.body.name
+  return res.send(genre)
+})
+
+server.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find(genre => genre.id === parseInt(req.params.id))
+  if(!genre) return res.status(404).send('This genre was not found')
+
+  const index = genres.indexOf(genre) //return the position (index) of that object in the array.
+  
+  genres.splice(index,1) 
+  
+  return res.send(genre)
+})
 
 
 
