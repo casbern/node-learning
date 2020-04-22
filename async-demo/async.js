d0f39dbb9cf9b9cea8998d1a38cbc0aa077c756b
Cássia <cascrisbern@gmail.com>
2020-04-22 11:36:00 -0300

    asyn example

---
+++ async-demo/async.js
@@ -0,0 +1,7 @@
+console.log("Before")
+
+setTimeout(() => {
+  console.log("Reading a user from a database...")
+},3000)
+
+console.log("After")
---
+++ async-demo/index.js
@@ -0,0 +1,18 @@
+console.log("Before")
+
+const user = getUser(1)
+console.log(user) 
+
+console.log("After")
+
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log("Reading a user from a database...")
+
+    return { id: id, gitHubUsername: "mosh"}
+
+  },3000)
+
+  return 1
+}
