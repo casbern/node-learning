dcc52f30927252f4e4c7e3b4518d29308f3beb7b
Cássia <cascrisbern@gmail.com>
2020-04-27 16:50:21 -0300

    working with events

--- node-module-system/events2/app.js
+++ node-module-system/events2/app.js
@@ -11,13 +11,5 @@ myOrder.on("orderProcessed", (arg) => {
 myOrder.process()
 
 
-// Considerations:
-// Why when I do not pass an argument to the method process() in the logger file
-// when I execute this code, the message "Your order was processed" does not appear?
-
-// Then after placing an argumet there and calling it here "myOrder.process("Your result is:")", the console.log appears?
-
-//But the actually message "Your result is" will only appear if the console.log(message) is set in the logger file.
-
 
 
