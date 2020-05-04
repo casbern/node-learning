ca003a0e4383616da115748c7af09ec0dfed3ec1
Cássia <cascrisbern@gmail.com>
2020-05-04 10:22:30 -0300

    update a document using update first

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/playground') //returns a promise
+mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true, useUnifiedTopology: true}) //returns a promise
   .then( () => { console.log("Connected to MongoDB") })
   .catch( err => { console.error("Could not connect to MongoDB", err)})
 
@@ -24,39 +24,73 @@ mongoose.connect('mongodb://localhost/playground') //returns a promise
     })
 
     const result = await course.save()
-    console.log(result)
+    //console.log(result)
   }
 
-  //Querying documents
-  async function getCourses() {
-    const courses = await Course
-      .find({ author: "Mosh", isPublished: true })
-      .limit(10)
-      .sort({ name: 1 })
-      .select({ name:1, tags: 1 })
-    console.log(courses)
-  }
+  //* Querying documents
+  // async function getCourses() {
+  //   const courses = await Course
+  //     .find({ author: "Mosh", isPublished: true })
+  //     .limit(10)
+  //     .sort({ name: 1 })
+  //     .select({ name:1, tags: 1 })
+  //   console.log(courses)
+  // }
 
-  getCourses()
+  // getCourses()
 
-  //Update a Document
-  async function updateCourse(id) {
-    const course = await Course.findById(id)
+  //* Update a Document - QUERY FIRST
+  // async function updateCourse(id) {
+  //   const course = await Course.findById(id)
 
-    if(!course) return
+  //   if(!course) return
 
-    // course.isPublished = true
-    // course.author = "another author"
+  //   // course.isPublished = true
+  //   // course.author = "another author"
 
-    //or
+  //   //or
 
-    course.set({
-      isPublished: true,
-      author: 'another author'
-    })
+  //   course.set({
+  //     isPublished: true,
+  //     author: 'another author'
+  //   })
 
-    const result = await course.save()
-    console.log(result)
-  }
+  //   const result = await course.save()
+  //   console.log(result)
+  // }
+
+  //* Update a Document - UPDATE FIRST  
+  //* returns confirmation
+  // async function updateCourse(id) {
+  //   const result = await Course.update( {_id: id}, { $set: {
+  //     author: "Paloma",
+  //     isPublished: false
+  //   }})
+  //   console.log(result)
+  // }
+  
+  //* Update a Document - Get the document that was updated
+  //* returns the document before it was updated 
+  // async function updateCourse(id) {
+  //     const course = await Course.findByIdAndUpdate( id, {
+  //       $set: {
+  //       author: "New Course",
+  //       isPublished: true
+  //     }
+  //   })
+  //     console.log(course)
+  //   }
+
+  //* returns the updated object
+    async function updateCourse(id) {
+      const course = await Course.findByIdAndUpdate( id, {
+        $set: {
+        author: "New Course",
+        isPublished: true
+      }
+    }, { new: true })
+      console.log(course)
+    }
+
 
   updateCourse('5eac51561ac4c3b3577aa42f')
